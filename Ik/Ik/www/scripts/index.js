b0a686584eab3f792f9a088542cc620cc8cda694
Karim <karim.benmoussa33@gmail.com>
2016-12-22 13:01:56 +0100

    IK

---
+++ Ik/Ik/www/scripts/index.js
@@ -0,0 +1,39 @@
+﻿// Pour obtenir une présentation du modèle Vide, consultez la documentation suivante :
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Pour déboguer du code durant le chargement d'une page dans Ripple ou sur les appareils/émulateurs Android, lancez votre application, définissez des points d'arrêt, 
+// puis exécutez "window.location.reload()" dans la console JavaScript.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Gérer les événements de suspension et de reprise Cordova
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        
+            
+             var d = new Date();
+            var days = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"];
+            var day = d.getDate();
+            var months = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"];
+            var year = d.getFullYear();
+            var currentDate = days[d.getDay()]+'/'+ day+'/'+ months[d.getMonth()]+'/'+year;;
+ 
+
+                $('#caseDate').val(currentDate);
+            
+    };
+    
+
+
+
+    function onPause() {
+        // TODO: cette application a été suspendue. Enregistrez l'état de l'application ici.
+    };
+
+    function onResume() {
+        // TODO: cette application a été réactivée. Restaurez l'état de l'application ici.
+    };
+} )();
---
+++ Ik/Ik/www/scripts/visiteDuJour.js
@@ -0,0 +1,27 @@
+﻿var OpenWeatherAppKey = "3ed691fa06d1e0e011e4264e05d25e24";
+ 
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // Gérer les événements de suspension et de reprise Cordova
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+
+        $('#get-weather-btn').click(getWeatherWithZipCode);
+    };
+    function goToVisiteDuJour() {
+        window.location.href = "visiteDujour.html";
+    };
+
+    function onPause() {
+        // TODO: cette application a été suspendue. Enregistrez l'état de l'application ici.
+    };
+
+    function onResume() {
+        // TODO: cette application a été réactivée. Restaurez l'état de l'application ici.
+    };
+})();
